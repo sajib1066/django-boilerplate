30c917eeaa8ef1c5844bb5ef0ed975388c393a0c
Sajib Hossain <sajib1066@gmail.com>
2022-04-03 21:51:43 +0600

    add custom auth admin

---
+++ customauth/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from customauth.models import User, Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    max_num = 1
+    verbose_name = 'Profile'
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'email', 'accepted_terms', 'read_terms', 'is_staff',
+        'is_superuser', 'is_active', 'date_joined', 'last_updated',
+        'verified_email', 'last_login'
+    )
+    search_fields = ('email',)
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'verified_email')
+
+    inlines = (ProfileInline, )
