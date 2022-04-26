4fdb280c50a9220abce8cd6bb236c34743dae77c
Sajib Hossain <sajib1066@gmail.com>
2022-04-27 00:36:57 +0600

    update django admin url

--- django_boilerplate/urls.py
+++ django_boilerplate/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('management/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
 ]
