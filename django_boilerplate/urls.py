60b951af4fb077243fcf1167097abca879ed4aaf
sajib1066 <sajib1066@gmail.com>
2023-11-26 23:50:57 +0800

    add media url

--- django_boilerplate/urls.py
+++ django_boilerplate/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import HomePageView
 
@@ -25,3 +27,10 @@ urlpatterns = [
     path('auth/', include('customauth.urls')),
     path('dashboard/', include('dashboard.urls')),
 ]
+
+# serve media files in development environment --------------------------------
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
